6955e92b08d8f7450e60a1e534b62961267f70be
Usman <muhammadusman@pral.com.pk>
2023-05-30 12:45:05 +0500

    initial commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Usman on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
